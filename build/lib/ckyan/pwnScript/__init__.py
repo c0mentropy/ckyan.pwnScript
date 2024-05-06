92f3de6c6a9e25fec4b12bd58d2e5632f418dedc
comentropy-ckyan <2194762703@qq.com>
2024-05-06 14:30:13 +0800

    Initial commit

--- build/lib/ckyan/pwnScript/__init__.py
+++
@@ -1,6 +0,0 @@
-from .connect import *
-from .misc import *
-from .debugger import *
-from .ck_opcode import *
-from .ae64_opcode import *
-from .stack import *
--- build/lib/ckyan/pwnScript/util/__init__.py
+++
@@ -1,9 +0,0 @@
-def setNotImplementedError(function_name: str):
-    raise NotImplementedError(f"{function_name}() is missing code.")
-
-
-def str_to_float(string_value: str) -> float:
-    numeric_string = ''.join(filter(str.isdigit, string_value))  # 过滤出字符串中的数字部分
-    float_value = float(numeric_string)  # 将纯数字字符串转换为浮点数
-
-    return float_value
