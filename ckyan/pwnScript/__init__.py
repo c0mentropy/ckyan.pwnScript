dd7709cf26a38487d5e31f00ff747f0d28ce74fd
comentropy-ckyan <2194762703@qq.com>
2024-05-21 17:58:42 +0800

    AE64 is annotated by default

--- ckyan/pwnScript/__init__.py
+++ ckyan/pwnScript/__init__.py
@@ -2,6 +2,6 @@ from .connect import *
 from .misc import *
 from .debugger import *
 from .ck_opcode import *
-from .ae64_opcode import *
+# from .ae64_opcode import *
 from .stack import *
 from .awd import *
